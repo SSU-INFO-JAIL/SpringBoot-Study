8c1d2e7bac8174dd9f2596dc34177c1316c016c9
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-12-24 14:08:15 +0900

    feat: employees 테이블 연결 & REST API 배포

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "employees")
+public class Employee {
+    // Getters and Setters
+    @Id
+    private int emp_no;
+    private String first_name;
+    private String last_name;
+
+}
---
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    private final EmployeeRepository repository;
+
+    public EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return repository.findAll();
+    }
+}
