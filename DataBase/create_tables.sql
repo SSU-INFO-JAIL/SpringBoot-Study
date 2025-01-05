6d0bb1a263ce20f0b92482b39e6817f26d8cd7b4
Jiwoong <jwchoi179@soongsil.ac.kr>
2025-01-05 12:18:33 +0900

    chore: 샘플 데이터 추가

--- DataBase/create_tables.sql
+++ DataBase/create_tables.sql
@@ -1,14 +1,14 @@
 create table member
 (
-    member_id   char(8)  not null
+    member_id   char(5)  not null
         primary key,
-    member_name char(10) null,
+    member_name char(20) null,
     member_addr char(20) null
 );
 
 create table product
 (
-    product_name char(10)       not null
+    product_name char(20)       not null
         primary key,
     cost         decimal(10, 2) not null,
     make_date    date           null,
